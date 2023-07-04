459513389a37446c441f598ae38fa9645b58e9a1
TJ Mazeika <mazeika.tj@gmail.com>
2023-07-04 11:32:35 -0500

    Fail pipeline when python cmd fails

--- run.sh
+++ run.sh
@@ -4,7 +4,10 @@ set -e
 
 mkdir -p out
 FILENAME="out/`date '+%Y-%m-%d %H:%M:%S'`.json"
+
+set -o pipefail
 python3 -u main.py | tee "$FILENAME"
+set +o pipefail
 
 curl -X POST 'https://prqtulnbkrgpznjvscje.supabase.co/rest/v1/apartments' \
     -H "apikey: $SUPABASE_KEY" \
