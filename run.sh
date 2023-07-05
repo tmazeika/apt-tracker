b17155f41679dc10613f6e99a685aac19fafa07b
TJ Mazeika <mazeika.tj@gmail.com>
2023-07-05 12:56:46 -0500

    Update run.sh

--- run.sh
+++ run.sh
@@ -6,11 +6,11 @@ mkdir -p out
 FILENAME="out/`date '+%Y-%m-%d %H:%M:%S'`.json"
 
 set -o pipefail
-python3 -u main.py | tee "$FILENAME"
+python3 -u main.py | tee >(jq -s > "$FILENAME")
 set +o pipefail
 
-curl -X POST 'https://prqtulnbkrgpznjvscje.supabase.co/rest/v1/apartments' \
+curl "https://prqtulnbkrgpznjvscje.supabase.co/rest/v1/apartments" \
     -H "apikey: $SUPABASE_KEY" \
     -H "Authorization: Bearer $SUPABASE_KEY" \
     -H "Content-Type: application/json" \
-    -d "`cat "$FILENAME" | jq -s`"
+    -d "@$FILENAME"
