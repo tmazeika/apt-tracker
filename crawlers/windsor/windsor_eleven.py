88eb57e5f35836200c7def006694d7178c4d83b0
TJ Mazeika <mazeika.tj@gmail.com>
2023-08-25 21:46:11 -0500

    Remove Eleven

--- crawlers/windsor/windsor_eleven.py
+++
@@ -1,7 +0,0 @@
-from crawlers.windsor.windsor import WindsorCrawler
-import undetected_chromedriver as uc
-
-
-class WindsorElevenCrawler(WindsorCrawler):
-    def __init__(self, driver: uc.Chrome):
-        super().__init__(driver, 'https://www.elevenbywindsor.com', 'Eleven', 70)
